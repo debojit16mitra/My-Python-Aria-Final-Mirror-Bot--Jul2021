491ed69387e591f598dce0be75c5e61bf29ef372
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-07-08 23:29:24 +0530

    Update usage.py

--- bot/modules/usage.py
+++ bot/modules/usage.py
@@ -63,12 +63,12 @@ def dyno_usage(update, context):
             AppMinutes = math.floor(AppQuotaUsed % 60)
             
             sendMessage(
-                f"<b>Dyno Usage for</b> <code>{app.name}</code>:\n"
+                f"<b>⌚Dyno Usage for</b> <code>{app.name}</code>:\n"
                 f"• <code>{AppHours}</code> <b>Hours and</b> <code>{AppMinutes}</code> <b>Minutes - {AppPercent}%</b>\n\n"
-                "<b>Dyno Remaining this month:</b>\n"
+                "<b>⏰Dyno Remaining this month:</b>\n"
                 f"• <code>{hours}</code> <b>Hours and</b> <code>{minutes}</code> <b>Minutes - {quota_percent}%</b>\n\n"
-                "<b>Estimated Dyno Expired:</b>\n"
-                f"• <code>{day}</code> <b>Days</b>",
+                "<b>👉Estimated Dyno Expired:</b>\n"
+                f"• <code>{day}</code> <b>Days😐</b>",
                 context.bot,
                 update
             )
